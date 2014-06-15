a055d84a3dce8ad47d175baae55bef3b4a3b7aa2
Jacobo Martínez <jacobo.amn87@gmail.com>
2014-06-15 13:31:17 -0430

    haciendo las configuraciones de oscar

--- frobshop/settings.py
+++ frobshop/settings.py
@@ -8,10 +8,18 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
+# OSCAR import
+from oscar.defaults import *
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+# Oscar Path helper
+PROJECT_DIR = os.path.dirname(__file__)
+location = lambda x: os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), x)
+PY3 = sys.version_info >= (3, 0)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -26,17 +34,36 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SQL_DEBUG = True
 
-# Application definition
+# Application definition. The apps are declared as list ("[" - "]") and not as tuples as usual
 
-INSTALLED_APPS = (
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django.contrib.sites',
     'django.contrib.messages',
+    'django.contrib.admin',
+    'django.contrib.flatpages',
     'django.contrib.staticfiles',
-)
+    'django.contrib.sitemaps',
+]
+
+THIRD_PARTY_APPS = [
+    'south',
+    'compressor',
+] + get_core_apps()
+
+MY_APPS = []
+
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + MY_APPS
+
+from oscar import get_core_apps
+INSTALLED_APPS = INSTALLED_APPS + get_core_apps()
+
+SITE_ID = 1
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -45,6 +72,67 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    ## OSCAR
+    'django.middleware.transaction.TransactionMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
+    # Allow languages to be selected
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    # Ensure a valid basket is added to the request instance for every request
+    'oscar.apps.basket.middleware.BasketMiddleware',
+    # Enable the ProfileMiddleware, then add ?cprofile to any
+    # URL path to print out profile details
+    #'oscar.profiling.middleware.ProfileMiddleware',
+)
+
+# Add Oscar's custom auth backend so users can sign in using their email
+# address.
+AUTHENTICATION_BACKENDS = (
+    'oscar.apps.customer.auth_backends.EmailBackend',
+    'django.contrib.auth.backends.ModelBackend',
+)
+
+LOGIN_REDIRECT_URL = '/'
+APPEND_SLASH = True
+
+# Haystack settings
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.simple_backend.SimpleEngine',
+    },
+}
+# HAYSTACK_CONNECTIONS = {
+#     'default': {
+#         'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+#         'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+#     },
+# }
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.request",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages",
+    # Oscar specific
+    'oscar.apps.search.context_processors.search_form',
+    'oscar.apps.promotions.context_processors.promotions',
+    'oscar.apps.checkout.context_processors.checkout',
+    'oscar.apps.customer.notifications.context_processors.notifications',
+    'oscar.core.context_processors.metadata',
+)
+
+# Add another path to Oscar's templates.  This allows templates to be
+# customised easily.
+from oscar import OSCAR_MAIN_TEMPLATE_DIR
+
+TEMPLATE_DIRS = (
+    location('templates'),
+    OSCAR_MAIN_TEMPLATE_DIR,
 )
 
 ROOT_URLCONF = 'frobshop.urls'
@@ -59,6 +147,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+        'ATOMIC_REQUESTS': True
     }
 }
 
@@ -75,8 +168,148 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Includes all languages that have >50% coverage in Transifex
+# Taken from Django's default setting for LANGUAGES
+gettext_noop = lambda s: s
+LANGUAGES = (
+    ('en-gb', gettext_noop('British English')),
+    ('zh-cn', gettext_noop('Simplified Chinese')),
+    ('nl', gettext_noop('Dutch')),
+    ('it', gettext_noop('Italian')),
+    ('pl', gettext_noop('Polish')),
+    ('ru', gettext_noop('Russian')),
+    ('sk', gettext_noop('Slovak')),
+    ('pt-br', gettext_noop('Brazilian Portuguese')),
+    ('fr', gettext_noop('French')),
+    ('de', gettext_noop('German')),
+    ('ko', gettext_noop('Korean')),
+    ('uk', gettext_noop('Ukrainian')),
+    ('es', gettext_noop('Spanish')),
+    ('da', gettext_noop('Danish')),
+    ('ar', gettext_noop('Arabic')),
+    ('ca', gettext_noop('Catalan')),
+    ('cs', gettext_noop('Czech')),
+    ('el', gettext_noop('Greek')),
+)
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
+ROSETTA_STORAGE_CLASS = 'rosetta.storage.SessionRosettaStorage'
+ROSETTA_ENABLE_TRANSLATION_SUGGESTIONS = True
+ROSETTA_REQUIRES_AUTH = False
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = location("public/media")
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+#ADMIN_MEDIA_PREFIX = '/media/admin/'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = location('public/static')
+
+STATICFILES_DIRS = (
+    location('static/'),
+)
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+
+# ==============
+# Oscar settings
+# ==============
+
+from oscar.defaults import *
+
+# Meta
+# ====
+
+OSCAR_SHOP_TAGLINE = 'TIENDA VIRTUAL'
+
+OSCAR_RECENTLY_VIEWED_PRODUCTS = 20
+OSCAR_ALLOW_ANON_CHECKOUT = True
+
+# This is added to each template context by the core context processor.  It is
+# useful for test/stage/qa sites where you want to show the version of the site
+# in the page title.
+DISPLAY_VERSION = True
+
+
+# Order processing
+# ================
+
+# Some sample order/line status settings
+OSCAR_INITIAL_ORDER_STATUS = 'Pending'
+OSCAR_INITIAL_LINE_STATUS = 'Pending'
+OSCAR_ORDER_STATUS_PIPELINE = {
+    'Pending': ('Being processed', 'Cancelled',),
+    'Being processed': ('Processed', 'Cancelled',),
+    'Cancelled': (),
+    'Processed': (),
+}
+
+# LESS/CSS/statics
+# ================
+
+# We default to using CSS files, rather than the LESS files that generate them.
+# If you want to develop Oscar's CSS, then set USE_LESS=True and
+# COMPRESS_ENABLED=False in your settings_local module and ensure you have
+# 'lessc' installed.  You can do this by running:
+#
+#    pip install -r requirements_less.txt
+#
+# which will install node.js and less in your virtualenv.
+
+USE_LESS = False
+
+COMPRESS_ENABLED = True
+COMPRESS_PRECOMPILERS = (
+    ('text/less', 'lessc {infile} {outfile}'),
+)
+COMPRESS_OFFLINE_CONTEXT = {
+    'STATIC_URL': 'STATIC_URL',
+    'use_less': USE_LESS,
+}
+
+# We do this to work around an issue in compressor where the LESS files are
+# compiled but compression isn't enabled.  When this happens, the relative URL
+# is wrong between the generated CSS file and other assets:
+# https://github.com/jezdez/django_compressor/issues/226
+COMPRESS_OUTPUT_DIR = 'oscar'
+
+# Logging
+# =======
+
+LOG_ROOT = location('logs')
+# Ensure log root exists
+if not os.path.exists(LOG_ROOT):
+    os.mkdir(LOG_ROOT)
+
+# Sorl
+# ====
+
+THUMBNAIL_DEBUG = True
+
+# Use a custom KV store to handle integrity error
+THUMBNAIL_KVSTORE = 'oscar.sorl_kvstore.ConcurrentKVStore'
+
+# Django 1.6 has switched to JSON serializing for security reasons, but it does not
+# serialize Models. We should resolve this by extending the
+# django/core/serializers/json.Serializer to have the `dumps` function. Also
+# in tests/config.py
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
+
+# Try and import local settings which can be used to override any of the above.
+try:
+    from settings_local import *
+except ImportError:
+    pass
--- frobshop/urls.py
+++ frobshop/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
+#from django.contrib import admin
+from oscar.app import application
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'frobshop.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+urlpatterns = [
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'', include(application.urls))
+]
+
+# urlpatterns = patterns('',
+#     # Examples:
+#     # url(r'^$', 'frobshop.views.home', name='home'),
+#     # url(r'^blog/', include('blog.urls')),
+
+#     url(r'^admin/', include(admin.site.urls)),
+# )
